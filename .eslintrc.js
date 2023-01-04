6b8d732cd9e864e99f6ac2e6c9bcb10189d79e5b
unknown <djouwad@protonmail.com>
2023-01-05 00:33:27 +0100

    CSS up

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-multiple-empty-lines */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'airbnb-base',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'windows'],
+    },
+};
+
